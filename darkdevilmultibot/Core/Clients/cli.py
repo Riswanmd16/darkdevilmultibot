c360245ef535dae5e4189d8a3ed37352a89bd75c
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-01-17 18:10:55 +0530

    Add files via upload

---
+++ darkdevilmultibot/Core/Clients/cli.py
@@ -0,0 +1,17 @@
+from pyrogram import Client
+
+from config import (API_HASH, API_ID, BOT_TOKEN, STRING1, STRING2, STRING3,
+                    STRING4, STRING5)
+
+app = Client(
+    "darkdevilmultibot",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+ASS_CLI_1 = Client(STRING1, API_ID, API_HASH)
+ASS_CLI_2 = Client(STRING2, API_ID, API_HASH)
+ASS_CLI_3 = Client(STRING3, API_ID, API_HASH)
+ASS_CLI_4 = Client(STRING4, API_ID, API_HASH)
+ASS_CLI_5 = Client(STRING5, API_ID, API_HASH)
